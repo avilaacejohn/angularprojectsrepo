13bfaa4a56208c7cca63306c02d367baf2720412
cr54.ace <cr54.ace@enfrausaph.com>
2018-08-21 04:10:30 +0800

    iMatrix Display List Name

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,13 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 1, name: 'Junius Diamond Reyes' },
+  { id: 2, name: 'Jhun Orquin' },
+  { id: 3, name: 'Reychil Malicia' },
+  { id: 4, name: 'Aaron Nikko Gonzales' },
+  { id: 5, name: 'Jomari Ceballe' },
+  { id: 6, name: 'Marlon Macatuno' },
+  { id: 7, name: 'Gene Loverboy' },
+  { id: 8, name: 'Maben Rosete' }
+  { id: 9, name: 'Ace John Avila' },
 ];
